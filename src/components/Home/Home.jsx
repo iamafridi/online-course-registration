f0f4c4df9714ac68b0ec3ebbfa89512df9572440
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-09-30 00:12:17 +0600

    load data dynamically using map technique

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,17 +1,22 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
+import { useState } from 'react';
 import Cart from '../Cart/Cart';
 import "./Home.css"
 
 const Home = () => {
 
+    const [course, setCourse] = useState([]);
 
 
     useEffect(() => {
-fetch("./data.json")
-.then((res) => res.json())
-.then((data) => console.log(data))
+        fetch("./data.json")
+            .then((res) => res.json())
+            .then((data) => setCourse(data))
     }, []);
+
+    // console.log(course);
 
 
 
@@ -20,19 +25,28 @@ fetch("./data.json")
 
     return (
         <div className='container' >
-            <div className='card-container'>
-                <div className='card'>
-                    <div className='card-img'>
-                        <img className='photo' src="https://i.ibb.co/5BbvFv9/Rectangle-2-1.png" alt="" />
+            <div className='home-container'>
+                <div className="card-container">
+                    {
+                        course.map(courses => (<div  key={courses.id} className='card'>
+                            <div className='card-img'>
+                                <img className='photo' src={courses.cover_image} alt="" />
 
-                    </div>
-                    <h2>Anthony</h2>
-                    <p><small>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque fuga odio recusandae, vel ad quo doloremque quam ipsa officia aperiam, error voluptates</small></p>
-                    <div className='info'>
-                        <p>salary :</p>
-                        <p>Credit :</p>
-                    </div>
-                    <button className='card-btn'>Select</button>
+                            </div>
+                            <h2>{courses.title}</h2>
+                            <p><small>{courses.description}</small></p>
+                            <div className='info'>
+                                <p>salary : {courses.price} </p>
+                                <p>Credit : {courses.credit_hour}hr </p>
+                            </div>
+                            <button className='card-btn'>Select</button>
+                        </div>))
+                    }
+                </div>
+
+                <div className='cart'>
+                    <h1>This is cart</h1>
+
                 </div>
             </div>
         </div>
