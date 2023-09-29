14e0bf459db32ece1f0d689b84b19a39ac7194c0
Afridi Akbar Ifty <afridiakbarifty@gmail.com>
2023-09-30 03:22:36 +0600

    added credit hour and fixed some style

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,18 +2,29 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 // import "./Cart.css";
+import './Cart.css'
 
-const Cart = ({ selectedCourse }) => {
+const Cart = ({ selectedCourse,remaining,totalCost }) => {
     console.log(selectedCourse);
     return (
-        <div>
-            <h5>
+        <div className='container-div'>
+            
+            <h4> <span className='remaining '>Remaining Hour: </span>  {remaining}</h4>
+            <hr />
+
+            <h3 className='total-selected-course'>
                 Total Selected Course : {selectedCourse.length}
-            </h5>
+            </h3>
+            <hr />
+<h4 className='course-Name'>Course Name :</h4>
+
             {
                 selectedCourse.map((courses) => (
-                    <li key={courses.id}>{courses.title}</li>
+                    <ol className='olclass' key={courses.id}>{courses.title}</ol>
                 ))}
+
+<h4 className='total-hour'>Total Hour: {totalCost}</h4>
+
         </div>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,7 +8,10 @@ import "./Home.css"
 const Home = () => {
 
     const [course, setCourse] = useState([]);
-const [selectedCourse, setSelectedCourse] = useState([]);
+    const [selectedCourse, setSelectedCourse] = useState([]);
+    const [remaining, setRemaining] = useState(0);
+    const [totalCost, setTotalCost] = useState(0);
+
 
     useEffect(() => {
         fetch("./data.json")
@@ -18,46 +21,69 @@ const [selectedCourse, setSelectedCourse] = useState([]);
 
     // console.log(course);
 
-const handleSelectCourse = (courses) => {
-    const isExist = selectedCourse.find((item) => item.id == courses.id);
-    if(isExist){
-       return alert("You Have Already Selected The Course")
-    }
-    else{ 
-        setSelectedCourse([...selectedCourse, courses]);
-    }
-    
-};
-// console.log(selectedCourse);
+
+    const handleSelectCourse = (courses) => {
+        const isExist = selectedCourse.find((item) => item.id == courses.id);
+
+        // for price of the couse
+
+        let count = courses.credit_hour;
+        if (isExist) {
+            return alert("You Have Already Selected The Course")
+        }
+        else {
+
+            selectedCourse.forEach((item) => {
+                count += item.credit_hour;
+            });
+            // console.log(count);
+            const totalremainingCredit = 20 - count;
+
+            if (count > 20) {
+                return alert("You Can Enroll In More Than 20 Credits")
+            }
+            else {
+                setTotalCost(count);
+                setRemaining(totalremainingCredit);
+                setSelectedCourse([...selectedCourse, courses]);
+
+            }
+        }
+
+    };
+    // console.log(selectedCourse);
 
 
     return (
-<div className='container' >
-<div className='home-container'>
-<div className="card-container">
-    {
-        course.map(courses => (<div  key={courses.id} className='card'>
-            <div className='card-img'>
-                <img className='photo' src={courses.cover_image} alt="" />
+        <div className='container' >
+            <h1 className='course-title'>Course Registration</h1>
+            <div className='home-container'>
+                <div className="card-container">
+                    {
+                        course.map(courses => (<div key={courses.id} className='card'>
+                            <div className='card-img'>
+                                <img className='photo' src={courses.cover_image} alt="" />
 
+                            </div>
+                            <h2 className='card-title'>{courses.title}</h2>
+                            <p className='card-description'><small>{courses.description}</small></p>
+                            <div className='info'>
+                                <p>Price : ${courses.price} </p>
+                                <p>Credit : {courses.credit_hour}hr </p>
+                            </div>
+                            <button onClick={() => handleSelectCourse(courses)} className='card-btn'>Select</button>
+                        </div>))
+                    }
+                </div>
+
+                <div className='cart'>
+                    <Cart selectedCourse={selectedCourse}
+                        remaining={remaining}
+                        totalCost={totalCost}></Cart>
+
+                </div>
             </div>
-            <h2>{courses.title}</h2>
-            <p><small>{courses.description}</small></p>
-            <div className='info'>
-                <p>salary : ${courses.price} </p>
-                <p>Credit : {courses.credit_hour}hr </p>
-            </div>
-            <button onClick={() =>handleSelectCourse(courses)} className='card-btn'>Select</button>
-        </div>))
-    }
-</div>
-
-<div className='cart'>
-   <Cart selectedCourse={selectedCourse} ></Cart>
-
-</div>
-</div>
-</div>
+        </div>
     );
 };
 
